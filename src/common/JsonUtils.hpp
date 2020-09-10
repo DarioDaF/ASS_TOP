75b0a47e33bf386cd6462d490e563a04f9215492
DarioDaF <dario.fagotto@gmail.com>
2020-09-10 11:20:11 +0200

    Comments on Utils

--- src/common/JsonUtils.hpp
+++ src/common/JsonUtils.hpp
@@ -3,6 +3,9 @@
 
 #include <nlohmann/json.hpp>
 
+/**
+ * Returns def if j is a null json object
+ */
 template<typename T>
 T json_get_or_default(const nlohmann::json& j, T def) {
   return j.is_null() ? def : j.get<T>();
--- src/common/Utils.hpp
+++ src/common/Utils.hpp
@@ -6,6 +6,9 @@
 
 typedef int idx_t;
 
+/**
+ * Iterator for numeric type T to define ranges without backing arrays
+ */
 template<typename T>
 class NumberIterator {
   public:
@@ -29,6 +32,14 @@ class NumberIterator {
     T num;
 };
 
+/**
+ * This class is used to define an enumerable for example in for each iteration
+ * 
+ * Typical usage:
+ *   for(auto x : NumberRange<int>(10)) {
+ *     printf("%d\n", x);
+ *   }
+ */
 template<typename T>
 class NumberRange {
   public:
@@ -37,6 +48,9 @@ class NumberRange {
     NumberIterator<T> begin() { return NumberIterator(first); }
     NumberIterator<T> end() { return NumberIterator(last); }
 
+    /**
+     * Create an std::vector containing the range
+     */
     std::vector<T> Vector() { return std::vector<T>(begin(), end()); }
   private:
     T first, last;
@@ -48,6 +62,14 @@ enum strong_ordering {
   greater = 1
 };
 
+/**
+ * This function returns a vector containing the minimum elements (not only the first)
+ * 
+ * @param begin Begin iterator
+ * @param end End iterator
+ * @param pred Function-like entity that defines order returning strong_ordering (in seneral < 0, = 0, > 0)
+ * @param prov Function-like that prerpocesses the elements (mapped onto iterator)
+ */
 template<class _It, class _Pred, class _Prov>
 std::vector<typename std::iterator_traits<_It>::value_type> min_elements(_It begin, _It end, _Pred pred, _Prov prov) {
   using _Idx = typename std::iterator_traits<_It>::value_type;
@@ -75,30 +97,59 @@ std::vector<typename std::iterator_traits<_It>::value_type> min_elements(_It beg
   return minimumIdxs;
 }
 
+/**
+ * This function returns a vector containing the minimum elements (not only the first)
+ * 
+ * @param begin Begin iterator
+ * @param end End iterator
+ * @param pred Function-like entity that defines order returning strong_ordering (in seneral < 0, = 0, > 0)
+ */
 template<class _It, class _Pred>
 std::vector<typename std::iterator_traits<_It>::value_type> min_elements(_It begin, _It end, _Pred pred) {
   return min_elements(begin, end, pred, [](auto x) { return x; });
 }
 
+/**
+ * This function returns a vector containing the minimum elements (not only the first)
+ * 
+ * @param size Iterate trough index list with this size
+ * @param pred Function-like entity that defines order returning strong_ordering (in seneral < 0, = 0, > 0)
+ * @param prov Function-like that prerpocesses the elements (mapped onto iterator)
+ */
 template<class _Pred, class _Prov>
 std::vector<idx_t> min_elements(idx_t size, _Pred pred, _Prov prov) {
   return min_elements(NumberIterator(0), NumberIterator(size), pred, prov);
 }
 
+/**
+ * This function returns a vector containing the minimum elements (not only the first)
+ * 
+ * @param size Iterate trough index list with this size
+ * @param pred Function-like entity that defines order returning strong_ordering (in seneral < 0, = 0, > 0)
+ */
 template<class _Pred>
 std::vector<idx_t> min_elements(idx_t size, _Pred pred) {
   return min_elements(NumberIterator(0), NumberIterator(size), pred, [](auto x) { return x; });
 }
 
+/**
+ * Default comparison predicate
+ */
 template<typename T>
 T so_cmp(const T& x, const T& y) {
   return x - y;
 }
+/**
+ * Inverted comparison predicate
+ */
 template<typename T>
 T so_negcmp(const T& x, const T& y) {
   return y - x;
 }
 
+/**
+ * Returns map[key] if present or def otherwise
+ */
 template<typename _MAP>
 typename _MAP::mapped_type value_or_default(const _MAP map, const typename _MAP::key_type& key, const typename _MAP::mapped_type& def) {
   typename _MAP::const_iterator it = map.find(key);
