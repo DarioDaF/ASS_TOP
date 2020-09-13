1530c75900d7faa512b5e952fdb4161fbbbccc90
KevinKyoshu <kevin@Anon.localdomain>
2020-09-13 15:28:40 +0200

    add example of res and analysis .sh file

---
+++ exSolAnalysis.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# To take the best solution on which run the Local Search
+./plotter/bestTaker.py
+
+# To plot some analysis about the performance of the algorithms
+./plotter/solAnalysis.py
+
