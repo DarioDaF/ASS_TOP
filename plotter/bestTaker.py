4b49fa9bdd23becbc6bef9c39c8113879282c215
KevinKyoshu <kevin@Anon.localdomain>
2020-09-16 10:42:10 +0200

    fix and upload best version of solution taker and solanalizer py script

--- plotter/bestTaker.py
+++ plotter/bestTaker.py
@@ -1,9 +1,14 @@
 '''
   The function evaluate the best solution obtained from all the algorithms and take the best algorithm. Than 
-  save the best path for Local Search algortim. Also Write a new ".csv" file with the best solutions found.
+  save the best path for Local Search algortim. Also Write a new ".csv" file with the best solutions found and 
+  other useful informations.
 
   Usage:
-    python3 ./plotter/bestTaker.py
+    python3 ./plotter/bestTaker.py [bestVersion]
+
+    bestVersion: 
+        - GB : Greedy and Backtracking
+        - LS : Local Search
 
   Input files:
     - Sol[algorithm]#[version].csv : file ".csv" that contain the solution of one version of one algorithm.
@@ -20,14 +25,26 @@ import sys
 import csv
 import os
 
-solFile = ["./solutions/SolGreedy#1.csv", \
-           "./solutions/SolGreedy#2.csv", \
-           "./solutions/SolBacktracking#1.csv", \
-           "./solutions/SolBacktracking#2.csv"]
+def printAlg(n, idx, res, maxRes):
+  if(float(maxRes[idx][2]) == 0.0): 
+    return 0.0 
+  else: 
+    return 1 - float(res[n][idx][2]) / float(maxRes[idx][2])
 
-fileNumber = 6
-instancesNumber = 4
-res = [[ 0 for x in range(fileNumber)] for y in range(instancesNumber)]
+if(sys.argv[1] == 'GB'): 
+  solFile = ["./solutions/SolGreedy#1.csv", \
+             "./solutions/SolGreedy#2.csv", \
+             "./solutions/SolBacktracking#1.csv", \
+             "./solutions/SolBacktracking#2.csv"]
+else:
+  solFile = ["./solutions/SolLocalSearch#SA.csv", \
+             "./solutions/SolLocalSearch#HC.csv", \
+             "./solutions/SolLocalSearch#SD.csv", \
+             "./solutions/SolLocalSearch#TS.csv"]
+
+instancesNumber = 387
+fileNumber = 4
+res = [[ 0 for x in range(instancesNumber)] for y in range(fileNumber)]
 maxRes = []
 cnt = 0
 
@@ -48,44 +65,81 @@ for idx in range(len(res[1])):
   temp = [res[0][idx][2], res[1][idx][2], res[2][idx][2] , res[3][idx][2]]
   maxN = max(temp)
   index = temp.index(max(temp))
-  if(index == 0):
-    alg = "GR#1"
-  elif(index == 1):
-    alg = "GR#2"
-  elif(index == 2):
-    alg = "BT#1"
-  elif(index == 3):
-    alg = "BT#1"
+  if(sys.argv[1] == 'LS'): 
+    if(index == 0):
+      alg = "SA"
+    elif(index == 1):
+      alg = "HC"
+    elif(index == 2):
+      alg = "SD"
+    elif(index == 3):
+      alg = "TS"
+  else:
+    if(index == 0):
+      alg = "GR#1"
+    elif(index == 1):
+      alg = "GR#2"
+    elif(index == 2):
+      alg = "BT#1"
+    elif(index == 3):
+      alg = "BT#1"
   maxRes.append([res[1][idx][0], res[1][idx][1], maxN, res[index][idx][3], alg])
 
-# print(maxRes)
-
-path = './solutions/bestSolutions.csv'
-with open(path, mode='w') as csv_file:
-  fieldnames = ['instance', 'chaoOpt', 'result', "percOnOpt", 'algorithm']
+path = './solutions/bestSolutions/'
+if(not os.path.isdir(path)):
+  os.mkdir(path)
+with open(path + 'bestSol' + sys.argv[1] + '.csv', mode='w') as csv_file:
+  
+  if(sys.argv[1] == 'GB'):
+    fieldnames = ['instance', 'chaoOpt', 'result', "percOnOpt", 'bestAlgorithm',"dtGR#1", "dtGR#2", "dtBT#1", "dtBT#2", "dtMedium"]
+  else:
+    fieldnames = ['instance', 'chaoOpt', 'result', "percOnOpt", 'bestAlgorithm', "dtSA", "dtHC", "dtSD", "dtTS", "dtMedium"]
+      
   writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
   writer.writeheader()
   
   for idx in range(len(maxRes)):
-    writer.writerow({'instance' : maxRes[idx][0], 
-                     'chaoOpt' : maxRes[idx][1],
-                     'result' : maxRes[idx][2],
-                     'percOnOpt' : maxRes[idx][3],
-                     'algorithm' : maxRes[idx][4]})
+    name = maxRes[idx][0].split('"')
+    
+    if(sys.argv[1] == "GB"):
+      writer.writerow({'instance' : name[1], 
+                       'chaoOpt' : maxRes[idx][1],
+                       'result' : maxRes[idx][2],
+                       'percOnOpt' : maxRes[idx][3],
+                       'bestAlgorithm' : maxRes[idx][4],
+                       'dtGR#1' : printAlg(0, idx, res, maxRes),
+                       'dtGR#2' : printAlg(1, idx, res, maxRes),
+                       'dtBT#1' : printAlg(2, idx, res, maxRes),
+                       'dtBT#2' : printAlg(3, idx, res, maxRes),
+                       'dtMedium' : (printAlg(0, idx, res, maxRes) + printAlg(1, idx, res, maxRes) + printAlg(2, idx, res, maxRes) + printAlg(3, idx, res, maxRes)) / 4})
+    else:
+      writer.writerow({'instance' : name[1], 
+                       'chaoOpt' : maxRes[idx][1],
+                       'result' : maxRes[idx][2],
+                       'percOnOpt' : maxRes[idx][3],
+                       'bestAlgorithm' : maxRes[idx][4],
+                       'dtSA' : printAlg(0, idx, res, maxRes),
+                       'dtHC' : printAlg(1, idx, res, maxRes),
+                       'dtSD' : printAlg(2, idx, res, maxRes),
+                       'dtTS' : printAlg(3, idx, res, maxRes),
+                       'dtMedium' : (printAlg(0, idx, res, maxRes) + printAlg(1, idx, res, maxRes) + printAlg(2, idx, res, maxRes) + printAlg(3, idx, res, maxRes)) / 4})
 
-solPath = "./outputs/routeHops/bestRoutes/"
-if(not os.path.isdir(solPath)):
-  os.mkdir("./outputs/routeHops/bestRoutes/")
 
-for idx in range(len(maxRes)):
-  fileName = maxRes[idx][0].split(".txt")
-  
-  if(maxRes[idx][4] == "BT#1"):
-    pathFrom = "./outputs/routeHops/backtracking/#1/" +  fileName[0] + ".out"
-  elif(maxRes[idx][4] == "BT#2"):
-    pathFrom = "./outputs/routeHops/backtracking/#2/" +  fileName[0] + ".out"
-  elif(maxRes[idx][4] == "GR#1"):
-    pathFrom = "./outputs/routeHops/greedy/#1/" +  fileName[0] + ".out"
-  elif(maxRes[idx][4] == "GR#2"):
-    pathFrom = "./outputs/routeHops/greedy/#2/" +  fileName[0] + ".out"
-  shutil.copy(pathFrom, solPath)
+if(sys.argv[1] == 'GB'): 
+  solPath = "./outputs/routeHops/bestRoutes/"
+  if(not os.path.isdir(solPath)):
+    os.mkdir("./outputs/routeHops/bestRoutes/")
+
+  for idx in range(len(maxRes)):
+    fileName1 = maxRes[idx][0].split(".txt")
+    fileName2 = fileName1[0].split('"')
+    
+    if(maxRes[idx][4] == "BT#1"):
+      pathFrom = "./outputs/routeHops/backtracking/#1/" +  fileName2[1] + ".out"
+    elif(maxRes[idx][4] == "BT#2"):
+      pathFrom = "./outputs/routeHops/backtracking/#2/" +  fileName2[1] + ".out"
+    elif(maxRes[idx][4] == "GR#1"):
+      pathFrom = "./outputs/routeHops/greedy/#1/" +  fileName2[1] + ".out"
+    elif(maxRes[idx][4] == "GR#2"):
+      pathFrom = "./outputs/routeHops/greedy/#2/" +  fileName2[1] + ".out"
+    shutil.copy(pathFrom, solPath)
